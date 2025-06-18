dc6cc63ac624cbe471f57ef95c526f60b96a2988
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-06-18 18:16:20 +0300

    Update hrneo-web.sh

--- hrneo-web.sh
+++ hrneo-web.sh
@@ -558,6 +558,7 @@ ln -sf /opt/etc/init.d/S99hrneo /opt/bin/hr
 ln -sf /opt/etc/init.d/S80lighttpd /opt/bin/php
 chmod +x "$INDEX_FILE"
 /opt/etc/init.d/S80lighttpd enable
+/opt/etc/init.d/S80lighttpd stop
 /opt/etc/init.d/S80lighttpd restart
 echo "[*] Установка завершена."
 echo "[*] Установщик веб панели удален."
